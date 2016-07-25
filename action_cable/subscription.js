94309ba3c80e5464901482d61598f46df272dfc2
Tom Chinery <hello@tomchinery.com>
2016-07-25 15:55:36 +0100

    Add: Subscription

---
+++ action_cable/subscription.js
@@ -0,0 +1,47 @@
+var Base = require('basejs')
+var ActionCable = require('../action_cable.js')
+
+Subscription = Base.extend({
+  constructor: function (consumer, params, mixin) {
+    this.consumer = consumer;
+    if (params == null) {
+      params = {};
+    }
+    this.identifier = JSON.stringify(params);
+    this.extendIt(this, mixin);
+  },
+
+  perform: function (action, data) {
+    if (data == null) {
+      data = {};
+    }
+    data.action = action;
+    return this.send(data);
+  },
+
+  send: function (data) {
+    return this.consumer.send({
+      command: "message",
+      identifier: this.identifier,
+      data: JSON.stringify(data)
+    });
+  },
+
+  unsubscribe: function() {
+    return this.consumer.subscriptions.remove(this);
+  },
+
+  extendIt: function (object, properties) {
+    var key, value;
+    if (properties != null) {
+      for (key in properties) {
+        value = properties[key];
+        object[key] = value;
+      }
+    }
+    return object;
+  }
+
+});
+
+module.exports = { Subscription: Subscription }
